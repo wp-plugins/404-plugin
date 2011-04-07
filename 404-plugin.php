c82ebc26c7ac8c5de9421b5bb6a19e35c3015192
404plugin <404plugin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-07 21:35:28 +0000

    git-svn-id: https://plugins.svn.wordpress.org/404-plugin/trunk@370364 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 404-plugin.php
+++ 404-plugin.php
@@ -1,9 +1,9 @@
 <?
 /*
 Plugin Name: 404 SEO Plugin
-Version: 1.5
+Version: 2.0
 Plugin URI: http://www.404plugin.com
-Description: Give yourself an SEO boost! Replaces 404 error with suggestions of related pages on the site. After installing, <a href='theme-editor.php'>go here</a>, and add &lt;?custom404_print404message();?&gt; to your 404 Template where you want the suggestions to appear.
+Description: Give yourself an SEO boost! Replaces 404 error with suggestions of related pages on the site. After installing, <a href='theme-editor.php'>go here</a>, and add &lt;?custom404_print404message();?&gt; to your 404 Template where you want the suggestions to appear. Version 2 gives cleaner, improved search results.
 Author: 404 Plugin
 Author URI: http://www.404plugin.com
 */
@@ -17,96 +17,56 @@ function custom404_print404message()
   $custom404_missingpagestr = str_replace(' ', '+', $custom404_missingpagestr);
   $custom404_headline =  'Related pages on this site';
   $custom404_server[] =  "+site%3A$_SERVER[SERVER_NAME]";
-  $custom404_googleresults = custom404_gettextbetweentags('<div id=res class=med>', '<div style="height:1px;line-height:0">', custom404_enginesearch('google'));
-  $custom404_googleresults = str_replace('a href="/', 'a target=_blank rel=nofollow href="http://www.google.com/', $custom404_googleresults);
-  $custom404_googleresults = str_replace('Tip: Save time by hitting the return key instead of clicking on "search"', '', $custom404_googleresults);
-  $custom404_googleresults = str_replace('Search Results', '', $custom404_googleresults);
-  for ($count=1; $count<15; $count++)
+  //new version
+  $custom404_googleresults = custom404_getgoogleresults($custom404_missingpagestr, $_SERVER[SERVER_NAME]);
+  
+  if (!($custom404_googleresults))
   {
-    $custom404_nofollow = $count-1 ? 'rel=nofollow' : '';
-    $custom404_googleresults = str_replace("onmousedown=\"return clk(this.href,'','','res','$count','')\"", $custom404_nofollow, $custom404_googleresults);
-    $custom404_googleresults = str_replace("onmousedown=\"return clk(this.href,'','','clnk','$count','')\"", $custom404_nofollow, $custom404_googleresults);
-  }
-  if (strpos($custom404_googleresults, 'did not match any documents.') || strpos($custom404_googleresults, $_SERVER[REDIRECT_URL] ))
-  {
-      $custom404_headline = 'No local results found. From the web:';
-      $custom404_googleresults = custom404_gettextbetweentags('<div id=res class=med>', '<div style="height:1px;line-height:0">', custom404_enginesearch('google', 'www'));
-      $custom404_googleresults = str_replace('a href="/', 'a target=_blank rel=nofollow href="http://www.google.com/', $custom404_googleresults);
-      $custom404_googleresults = str_replace('Tip: Save time by hitting the return key instead of clicking on "search"', '', $custom404_googleresults);
-      $custom404_googleresults = str_replace('Search Results', '', $custom404_googleresults);
-      for ($count=1; $count<15; $count++)
+      $custom404_headline = 'No local results found. From the web:';  
+	
+      $custom404_googleresults = custom404_getgoogleresults($custom404_missingpagestr);
+      $custom404_yahooresults = custom404_getyahooresults($custom404_missingpagestr);	  
+	  $custom404_yahooresultsarr = explode("\n", $custom404_yahooresults);
+	  $custom404_formattedyahooresults = '';
+	  foreach ($custom404_yahooresultsarr as $custom404_yahooresult)
+	  { 
+	    if (strlen(trim($custom404_yahooresult)))
+	      if (strpos($custom404_googleresults, $custom404_yahooresult) === false && 
+		  strpos($custom404_googleresults, str_replace('rel=nofollow', '', $custom404_yahooresult)) === false )
+		    $custom404_formattedyahooresults .= "$custom404_yahooresult\n";
+	  }
+	  
+      if (!($custom404_googleresults) && !($custom404_formattedyahooresults))
       {
-        $custom404_nofollow = $count-1 ? 'rel=nofollow' : '';
-        $custom404_googleresults = str_replace("onmousedown=\"return clk(this.href,'','','res','$count','')\"", $custom404_nofollow, $custom404_googleresults);
-        $custom404_googleresults = str_replace("onmousedown=\"return clk(this.href,'','','clnk','$count','')\"", $custom404_nofollow, $custom404_googleresults);
-      }
-
-      $custom404_googleurls = custom404_findurls($custom404_googleresults);
-      $count=1;
-      foreach ($custom404_googleurls as $custom404_googleurl)
-      {
-        $custom404_nofollow = $count-1 ? 'rel=nofollow' : '';
-
-
-        if (!(strpos($custom404_googleurl, '.google.com')) && !(strpos($custom404_googleurl, 'search?')) )
-        {
-          $custom404_snippet = custom404_gettextbetweentags($custom404_googleurl, '</div>', $custom404_googleresults . '<br>');
-          $custom404_snippet = custom404_gettextbetweentags('>', '<cite>', $custom404_snippet);
-          $custom404_snippet = str_replace('<', ' <', $custom404_snippet);
-          $custom404_snippet = strip_tags($custom404_snippet);
-          $custom404_snippet = str_replace('  ', ' ', $custom404_snippet);
-          $custom404_snippet = str_replace('...', '', $custom404_snippet);
-          if(!(strpos($custom404_snippet, 'ached - Similar')))
-          {
-            $custom404_punctuation = array('.', '?', '!', ':', '-', '|');
-            $custom404_closetag = array('</a>.', '</a>?', '</a>!', '</a>:', '</a>-', '</a>|');
-            $custom404_snippet = str_replace($custom404_punctuation, $custom404_closetag, $custom404_snippet);
-            $custom404_snippetparts = explode('</a>', $custom404_snippet);
-            $firstpart = array_shift($custom404_snippetparts);
-            $otherparts = @implode('', $custom404_snippetparts);
-            $custom404_snippet = $firstpart . '</a>' . $otherparts;
-            $custom404_formattedgoogleurls[]  = "<a href='$custom404_googleurl' $custom404_nofollow>$custom404_snippet";
-                   $count++;
-          }
-        }
-      }
-
-      $custom404_formattedgoogleresults = @implode("<br>\n<br>\n", $custom404_formattedgoogleurls);
-      if (strpos($custom404_googleresults, 'did not match any documents.'))
-      {
-          $custom404_googleresults = '<ol>No results found on the web.</ol>';
+          $custom404_googleresults = '<br>No results found on the web.';
       }
       else
       {
-          $custom404_googleresults = $custom404_formattedgoogleresults;
-      }
+	      $custom404_googleresults = ($custom404_googleresults == '0') ? '' : $custom404_googleresults;
+	      $custom404_formattedyahooresults = ($custom404_formattedyahooresults == '0') ? '' : $custom404_formattedyahooresults; 
+          $custom404_googleresults = utf8_encode($custom404_googleresults );
+		  $custom404_formattedyahooresults = utf8_encode($custom404_formattedyahooresults );
+	  }
   }
-  $custom404_yahooresults = utf8_encode(custom404_gettextbetweentags('<ol start="1">',  '</ol>', custom404_enginesearch('yahoo')));
-  $custom404_yahooresults = str_replace('<h2>WEB RESULTS</h2>', '', $custom404_yahooresults);
-  if (strpos($custom404_yahooresults, 'We did not find results for'))
-  {
-    $custom404_yahooresults = utf8_encode(custom404_gettextbetweentags('<ol start="1">',  '</ol>', custom404_enginesearch('yahoo', 'www')));
-    $custom404_yahooresults = str_replace('<h2>WEB RESULTS</h2>', '', $custom404_yahooresults);
-    if (strpos($custom404_yahooresults, 'We did not find results for'))
-      {
-          $custom404_yahooresults = 'No results found on the web.';
-      }
-  }
+  $custom404_googleresults = $custom404_headline . "<br>" . $custom404_googleresults;
+  
   $custom404_formattedquerystring = $_SERVER[REDIRECT_QUERY_STRING] ? "?$_SERVER[REDIRECT_QUERY_STRING]":"";
   print "<iframe name='404pluginV15' src='http://www.404plugin.com/track.php' scrolling=no height=1 width=1 marginheight=0 marginwitdh=0 frameborder=0></iframe>
    <table border=0 width=100%>
+  <tr>
+   <td valign=top>
    <h3>Suggestions</h3>
    <!--Google results-->
    $custom404_googleresults
-   </td>
-   </tr><tr>
-   <td valign=top>
+  
    <!--Yahoo results-->
-   <ol>" .
-  // $custom404_yahooresults .
-   "</ol>
+   
+   $custom404_formattedyahooresults
+   
    </td></tr>
-   </table>";
+   </table>
+   
+   <!--404Plugin 2.0 released 4.2011-->";
 }
 
 function custom404_findurls($Page)
@@ -155,28 +115,18 @@ function custom404_getmissingpagestr()
   return $custom404_missingpagestr;
 }
 
-function custom404_enginesearch($engine='google', $www=0)
+function custom404_loadpage($url)
 {
-  global $custom404_server;
-  global $custom404_missingpagestr;
-  $header  = array();
-  $referers = array('google' => 'Referer: http://www.google.com', 'yahoo' => 'http://www.yahoo.com');
-  array_push ($header, $referers[$engine]);
-  if($www)$custom404_server[0] = str_replace('+','+-', $custom404_server[0]);
   $UserAgents = array("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1",
                       "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)");
-  $URLs = array ('google' => "http://www.google.com/search?hl=en&q=$custom404_missingpagestr$custom404_server[0]&btnG=Google+Search&aq=f&oq=",
-                 'yahoo' => "http://search.yahoo.com/search?p=$custom404_missingpagestr$custom404_server[0]&fr=yfp-t-501&toggle=1&cop=mss&ei=UTF-8");
-
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_USERAGENT, $UserAgents[array_rand($UserAgents)]);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_URL, $URLs[$engine]);
+  curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_COOKIESESSION, 1);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
   $ret = curl_exec($ch);
   curl_close($ch);
-  if (!strlen($ret)) $ret = ("An error occured when loading the $engine search results.");
+  if (!strlen($ret)) $ret = ("An error occured.");
   return $ret;
 }
 
@@ -187,6 +137,135 @@ function custom404_gettextbetweentags ($start, $end, $string)
     $foundstring = trim (substr($string, $startpos, $matchlength));
     return $foundstring;
 }
+
+function custom404_getgoogleresults($term, $domain=0)
+{ 
+  if ($domain) {$siteclause = "+site%3A$domain";}
+  
+  $position = 1;
+  libxml_use_internal_errors(true);
+  $allresults = array();
+  $pages = 2;
+  $textresults='';
+  $totalfound = 0;
+ 
+  for ($currentpage=1;$currentpage<=$pages;$currentpage++)
+  {  
+    $pagedom = new DOMDocument();
+    $start = ($currentpage - 1) * 10;
+    $startclause = $start ? "&start=$start" : '';
+    $googleurl = "http://www.google.com/search?hl=en&source=hp&q=" . urlencode($term) . $siteclause . $startclause . "&gl=us&pws=0";
+	//print $googleurl;
+	$pagedom->loadHTMLFile($googleurl);
+    
+    $spans = $pagedom->getElementsByTagName("span");
+    foreach($spans as $span)
+      if ($span->getAttribute('class') == 'f' || $span->getAttribute('class') == 'gl')
+        $span->nodeValue = '';
+    $divs = $pagedom->getElementsByTagName("div");
+    foreach($divs as $div)
+      if ($div->getAttribute('class') == 'osl')
+        $div->nodeValue = '';
+        
+    $results = $pagedom->getElementsByTagName("h3");
+    
+	foreach ($results as $result)
+    {
+      if ($result->getAttribute("class") == "r")
+      {
+        $children = $result->childNodes;
+        foreach ($children as $child)
+        {
+          if (is_object($child) && $child->getAttribute("class") == "l" && $position <=10)
+          { //initialize variables
+            $anchor = $child->nodeValue;
+            $url = $child->getAttribute("href");
+            $urlparts = explode('/', $url);
+            $domain = $urlparts[2];
+            
+            //description from google search results
+            $parent = $result->parentNode;
+            $siblings = $parent->childNodes;
+            $description = '';
+            foreach($siblings as $sibling)
+            { //var_dump($uncle);
+                if (@$sibling->getAttribute("class") == "s")
+                {
+                  $shorturl = str_replace('http://', '', $url);  
+                  $description = custom404_gettextbetweentags("STARTHERE", $shorturl, "$STARTHERE$sibling->nodeValue");              
+                }  
+            }
+			$nofollow = $position == 1 ? '' : 'rel=nofollow';
+			$textresults .=  "<b><a $nofollow href='$url'>$anchor</a></b><br>";
+			
+			$position++; $totalfound++;
+		  }
+		}
+	  }
+	}
+  }  
+  $textresults = $totalfound ? $textresults : 0;
+  
+  return $textresults;
+}
+
+function custom404_getyahooresults($key_words, $num_results=10)
+{
+    global $custom404_server;
+    $results = '';
+    $terms=str_replace(' ','+',$key_words);
+    $pages=$num_results/10;
+
+    for($i=0;$i<$pages;$i++){
+
+    	$b=10*$i+1;
+    	$serpurl="http://search.yahoo.com/search?p=$terms&pstart=1&b=$b";
+    	$page_results=custom404_getyahooserp($serpurl);
+    	if ($page_results)
+		  $results .= $page_results;
+    }
+    if (strlen($results) == 0) $results=0;
+    return $results;
+}
+
+function custom404_getyahooserp($serpurl)
+{ 
+    $position = 11;
+    $serpOriginal = custom404_loadpage($serpurl);
+    $results='';
+    if (strpos($serpOriginal,"We did not find results for") ===FALSE){
+	    $serpArray = explode("<a class=\"yschttl spt\"",$serpOriginal);
+	    array_shift($serpArray);
+
+	    foreach($serpArray as $key=>$result){
+
+		    $url='http://'.strip_tags(custom404_gettextbetweentags('<span class=url>','</span>',$result));
+		    if (strpos($url,'...')){
+		    	$url=custom404_gettextbetweentags('href="','"',$result);
+		    	if (strpos($url,'yahoo.com/click')){
+		    		$url=custom404_gettextbetweentags('?u=','&', urldecode($url) );
+		    	}
+		    	elseif (strpos($url,'rds.yahoo.com')){ //convert urls like        //http://rds.yahoo.com/_ylt=A0oGkmdAhmRJnqsAr9FXNyoA;_ylu=X3oDMTBydHRjbmRzBHNlYwNzcgRwb3MDMwRjb2xvA3NrMQR2dGlkAw--/SIG=137q03skj/EXP=1231411136/**http%3a//commitments.clintonglobalinitiative.org/projects.htm%3fmode=view%26rid=43102
+		    		$url=custom404_gettextbetweentags('**','', urldecode($url) );
+		    	}
+		    }
+		    $title=htmlspecialchars_decode(strip_tags(custom404_gettextbetweentags('>','</div>',$result)));
+		    if (strpos($result,'<div class="abstr">'))
+              $description = custom404_gettextbetweentags('<div class="abstr">','</div>',$result);
+		    elseif (strpos($result,'<div class="sm-abs">'))
+              $description = custom404_gettextbetweentags('<div class="sm-abs">','</div>',$result);
+            // $title = str_replace('Warning: Dangerous Downloads', '', $title);
+           if (!(strpos($url, 'ttp://news.search.yahoo.com')) && $position <= 20) //filter news results away
+             {
+			   $results .="<b><a rel=nofollow href='$url'>$title</a></b><br>\n";
+	           $position++;
+			 }
+		}
+	}
+	if (strlen($results) == 0) $results = 0;
+	return $results;
+}
+
 
 //Override SEO Plugin Title - Comment these lines out to restore.
 add_option("aiosp_404_title_format", custom404_getmissingpagestr(), 'All in One SEO Plugin 404 Title Format', 'yes');
